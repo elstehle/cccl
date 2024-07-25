9caa04d9641a645c7499918830ea9d60992aa717
Elias Stehle <3958403+elstehle@users.noreply.github.com>
2024-07-24 23:13:28 -0700

    benchmarks all offset types

--- cub/benchmarks/bench/scan/exclusive/by_key.cu
+++ cub/benchmarks/bench/scan/exclusive/by_key.cu
@@ -170,7 +170,7 @@ using value_types =
                      >;
 #endif // TUNE_ValueT
 
-NVBENCH_BENCH_TYPES(scan, NVBENCH_TYPE_AXES(key_types, value_types, some_offset_types))
+NVBENCH_BENCH_TYPES(scan, NVBENCH_TYPE_AXES(key_types, value_types, offset_types))
   .set_name("base")
   .set_type_axes_names({"KeyT{ct}", "ValueT{ct}", "OffsetT{ct}"})
   .add_int64_power_of_two_axis("Elements{io}", nvbench::range(16, 28, 4));
